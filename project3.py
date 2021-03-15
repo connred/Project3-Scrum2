a139c776f9082521bf27ba08f7f88e4b6c92ccdf
Connor Redmond <connred@icloud.com>
2021-03-15 17:13:48 -0500

    api func work

--- project3.py
+++ project3.py
@@ -5,6 +5,52 @@ import requests
 
 api_key = "65210ZZ38CVFIWM4"
 #api docs https://www.alphavantage.co/documentation/
+def getData(time_series, symbol, api_key):
+    #Daily Time Series API Func
+    #https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo
+    #TIME_SERIES = TIME_SERIES_DAILY
+    #symbol = symbol 
+    #apikey = api_key
+
+    #Intraday Time Series API Func
+    #https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo
+    #time_series = TIME_SERIES_INTRADAY
+    #symbol = symbol
+    #apikey = api_key
+    #interval = interaval? api required interval 
+
+    #Weekly Time Series API Func
+    #https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=IBM&apikey=demo
+    #time_series = TIME_SERIES_WEEKLY
+    #symbol = symbol 
+    #apikey = api_key 
+
+    #Monthly Time Series API Func
+    #https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=IBM&apikey=demo
+    #time_series = TIME_SERIES_MONTHLY
+    #symbol = symbol
+    #apikey = api_key
+    #create string
+    
+    ##STRING ADDITION NOT WORKING, probably doing it wrong 
+    apistring = "https://www.alphavatage.co/query?function="
+    if (time_series == 1):
+        time_series = "TIME_SERIES_INTRADAY"
+    elif (time_series == 2):
+        time_series = "TIME_SERIES_DAILY"
+    elif (time_series == 3):
+        time_series = "TIME_SERIES_WEEKLY"
+    elif (time_series == 4):
+        time_series = "TIME_SERIES_MONTHLY"
+
+    apistring += (time_series + "&symbol=" + symbol)
+    if (time_series == "TIME_SERIES_INTRADAY"):
+        apistring += "&interval=30min"
+    apistring +="&apikey=" + api_key
+    
+    apistring = "https://www.alphavatage.co/query?function="
+    print(apistring)
+
 print("Stock Data Visualizer\n======================")
 
 symbol = input("\nEnter the stock symbol are looking for: ")
@@ -33,31 +79,9 @@ start_date = input("Enter the start Date (YYYY-MM-DD):")
 end_date = input("Enter the end Date (YYYY-MM-DD):")
 # error hadling > check valid date in YYYY-MM-DD and that it is after the start date
 
+getData(time_series,symbol,api_key)
 
-#Daily Time Series API Func
-#https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo
-#TIME_SERIES = TIME_SERIES_DAILY
-#symbol = symbol 
-#apikey = api_key
 
-#Intraday Time Series API Func
-#https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=IBM&interval=15min&slice=year1month1&apikey=demo
-#time_series = TIME_SERIES_INTRADAY
-#symbol = symbol
-#apikey = api_key
-#interval = interaval? api required interval 
-
-#Weekly Time Series API Func
-#https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=IBM&apikey=demo
-#time_series = TIME_SERIES_WEEKLY
-#symbol = symbol 
-#apikey = api_key 
-
-#Monthly Time Series API Func
-#https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=IBM&apikey=demo
-#time_series = TIME_SERIES_MONTHLY
-#symbol = symbol
-#apikey = api_key
 
 #parse out json data that comes back from API
 
